1a445dac53f73be8bd31bdbec7d7ddfc4af18b61
abdelrahman97 <33758629+AbdoElkashef@users.noreply.github.com>
2017-11-29 03:41:35 +0200

    Add files via upload

--- Text.asm
+++ Text.asm
@@ -3,60 +3,45 @@
 INCLUDE io.h
 .STACK 4096
 .DATA
-w1 DWORD ?
-w2 DWORD ?
-w3 DWORD ?
-w4 DWORD ?
-s BYTE "enter the weight and grade ",0
-a BYTE 40 DUP(?)
-b BYTE 11 DUP (?),0
-d BYTE "wighted sun is ",0
-e BYTE " sum of wieght is ",0
-f BYTE "  wieghted average",0
+a DWORD ?
+b DWORD ?
+o DWORD ?
+d DWORD ?
+e BYTE "enter the num ",0
+f BYTE " the sum is ",0
+g BYTE " the average ",0
+h BYTE 40 DUP(?)
+k BYTE 11 DUP(?)
+BYTE "."
+y BYTE 11 DUP(?),0
 .CODE
 MainProc PROC
-input s,a,40
-atod a
-mov w1,eax
-input s,a,40
-atod a
-imul eax,w1
-mov ebx,eax
-input s,a,40
-atod a
-mov w2,eax
-input s,a,40
-atod a
-imul eax,w2
-add ebx,eax
-input s,a,40
-atod a
-mov w3,eax
-input s,a,40
-atod a
-imul eax,w3
-add ebx,eax
-input s,a,40
-atod a
-mov w4,eax
-input s,a,40
-atod a
-imul eax,w4
-add ebx,eax
-dtoa b,ebx
-output d,b
-mov eax,w1
-add eax,w2
-add eax,w3
-add eax,w4
-dtoa b,eax
-output e,b
-xchg eax,ebx
+mov edx,0
+input e,h,40
+atod h
+mov a,eax
+input e,h,40
+atod h
+mov b,eax
+input e,h,40
+atod h
+mov o,eax
+input e,h,40
+atod h
+mov d,eax
+add eax,a
+add eax,b
+add eax,o
+dtoa y,eax
+output f,y
+mov ebx,4
 cdq
 idiv ebx
-dtoa b,eax
-output f,b
+dtoa k,eax
+dtoa y,edx
+output g,k
+
+
 mov eax,0
-ret
 MainProc ENDP
 END
